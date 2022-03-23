497858b245c298c6025a0c863be58d2b19f9a387
Ana Cervantes <anac@Anas-iMac.attlocal.net>
2022-03-23 16:52:04 -0700

    setup the mapbox map for the tour locations to display on the map

--- app.js
+++ app.js
@@ -44,7 +44,54 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Sets security http headers - helmet() will return a function This should go at the beginning.
-app.use(helmet());
+// app.use(helmet())
+// app.use(helmet({
+//   contentSecurityPolicy: {
+//     directives: {
+//       defaultSrc: ["'self'", 'data:', 'blob:', 'https:', 'ws:'],
+//       baseUri: ["'self'"],
+//       fontSrc: ["'self'", 'https:', 'data:'],
+//       scriptSrc: [
+//         "'self'",
+//         'https:',
+//         'http:',
+//         'blob:',
+//         'https://*.mapbox.com',
+//         'https://js.stripe.com',
+//         'https://m.stripe.network',
+//         'https://*.cloudflare.com',
+//       ],
+//       frameSrc: ["'self'", 'https://js.stripe.com'],
+//       objectSrc: ["'none'"],
+//       styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
+//       workerSrc: [
+//         "'self'",
+//         'data:',
+//         'blob:',
+//         'https://*.tiles.mapbox.com',
+//         'https://api.mapbox.com',
+//         'https://events.mapbox.com',
+//         'https://m.stripe.network',
+//       ],
+//       childSrc: ["'self'", 'blob:'],
+//       imgSrc: ["'self'", 'data:', 'blob:'],
+//       formAction: ["'self'"],
+//       connectSrc: [
+//         "'self'",
+//         "'unsafe-inline'",
+//         'data:',
+//         'blob:',
+//         'https://*.stripe.com',
+//         'https://*.mapbox.com',
+//         'https://*.cloudflare.com/',
+//         'https://bundle.js:*',
+//         'ws://127.0.0.1:*/',
+
+//       ],
+//       upgradeInsecureRequests: [],
+//     },
+//   },
+// }));
 
 // // // // // BODY PARSER
 // limit that amount of data that comes in the body for security purposes.
