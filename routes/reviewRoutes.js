d701b37817ae692f2920a0800fb6b031f08bb764
Ana MacBook <anac2828@yahoo.com>
2022-03-23 17:47:56 -0700

    test

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as reviewController from '../controllers/reviewController.js';
 import * as authController from '../controllers/authController.js';
-
+// test
 // Will merge the tour and reviews end point to get access to the tour id
 const router = express.Router({ mergeParams: true });
 
